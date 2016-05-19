b5af04ac51f103eb6fc35f908500ad04c958a4d4
DJGuu <goncalo.correia@rupeal.com>
2016-05-19 16:22:16 +0100

    Inserted functions

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import requests
 import os.path
 import sys
 
-try:
-	account_name = "pawel-1"
-	invoice_id = "8927119"
+def readApiKey(fileName):
+	f = open("ix.key","r")
+	api_key = f.read()
+	return api_key
 
-	if os.stat('ix.key').st_size > 0:
-		f = open("ix.key","r")
-		api_key = f.read()
+def sendRequest(acc_name,iv_id,api_key):
+	r = requests.get("https://"+acc_name+".app.invoicexpress.com/invoices/"+iv_id+".xml?api_key="+api_key)
+	return r.text
 
-		r = requests.get("https://"+account_name+".app.invoicexpress.com/invoices/"+invoice_id+".xml?api_key="+api_key)
+account_name = "pawel-1"
+invoice_id = "8927119"
+ApiKeyFile = "ix.key"
 
-		print r.text
-
+if os.path.exists(ApiKeyFile):
+	if os.stat(ApiKeyFile).st_size > 0:
+		output = sendRequest(account_name,invoice_id,readApiKey(ApiKeyFile))
+		print output
+		
 	else:
 		print "Key file is Empty. Exiting"
-        sys.exit()
-
-except OSError:
+       	sys.exit()
+else:
     print "Key file not found. Exiting"
     sys.exit()
