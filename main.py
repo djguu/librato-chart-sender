f546df046b1d666bc2f97e1111b068d3b38412d4
DJGuu <goncalo.correia@rupeal.com>
2016-05-19 17:08:27 +0100

    added class

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import requests
 import os.path
 import sys
 
-def readApiKey(fileName):
-	f = open("ix.key","r")
-	api_key = f.read()
-	return api_key
+class librato_chart_sender():
+	def readApiKey(self, fileName):
+		if os.path.exists(fileName):
+			f = open("ix.key", "r")
+			api_key = f.read()
+			if(len(api_key) != 0):
+				return api_key
+			else:
+				return "Key file is empty. Exiting"
+				sys.exit()
+		else:
+			print "Key file not found. Exiting"
+	    	sys.exit()
 
-def sendRequest(acc_name,iv_id,api_key):
-	r = requests.get("https://"+acc_name+".app.invoicexpress.com/invoices/"+iv_id+".xml?api_key="+api_key)
-	return r.text
+	def sendRequest(self, acc_name, iv_id, api_key):
+		r = requests.get("https://" + acc_name + ".app.invoicexpress.com/invoices/" + iv_id + ".xml?api_key=" + api_key)
+		return r.text
 
-account_name = "pawel-1"
-invoice_id = "8927119"
-ApiKeyFile = "ix.key"
+	def main(self, acc_name, iv_id, apkeyfile):
+		api_key = self.readApiKey(apkeyfile)
+		output = self.sendRequest(acc_name, iv_id, api_key)
+		return output
 
-if os.path.exists(ApiKeyFile):
-	if os.stat(ApiKeyFile).st_size > 0:
-		output = sendRequest(account_name,invoice_id,readApiKey(ApiKeyFile))
-		print output
-		
-	else:
-		print "Key file is Empty. Exiting"
-       	sys.exit()
-else:
-    print "Key file not found. Exiting"
-    sys.exit()
+
+librato = librato_chart_sender().main("pawel-1", "8927119", "ix.key")
+print librato
